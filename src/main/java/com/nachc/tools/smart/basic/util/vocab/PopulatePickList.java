bb68181ef1064deb13c29bc1cff1b962fbb08b73
greshje <greshje@gmail.com>
2022-07-23 18:07:17 -0400

    work on parsing questionnaire

--- src/main/java/com/nachc/tools/smart/basic/util/vocab/PopulatePickList.java
+++ src/main/java/com/nachc/tools/smart/basic/util/vocab/PopulatePickList.java
@@ -1,13 +1,24 @@
 package com.nachc.tools.smart.basic.util.vocab;
 
+import java.util.List;
+
 import org.hl7.fhir.r4.model.Questionnaire;
+import org.hl7.fhir.r4.model.Questionnaire.QuestionnaireItemComponent;
 
 import com.nach.core.util.fhir.parser.r4.FhirR4JsonParser;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PopulatePickList {
 
 	public static void exec(String src) {
 		Questionnaire quest = FhirR4JsonParser.parse(src, Questionnaire.class);
+		List<QuestionnaireItemComponent> items = quest.getItem();
+		log.info("Got questionnaire: " + quest.getName());
+		for(QuestionnaireItemComponent item : items) {
+			log.info("\tGot item: " + item.getAnswerValueSet());
+		}
 	}
 
 }
